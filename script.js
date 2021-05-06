d7ce9b736402b3ca1dc98f97d828643a6e16f4b5
ViniciusMossi <vini_agg@hotmail.com>
2021-05-06 19:48:55 -0400

    Resolve requisito 5 e estilização

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const tarefa = document.getElementById('texto-tarefa');
 const botao = document.getElementById('criar-tarefa');
 const listadeTarefa = document.getElementById('lista-tarefas');
 
+// cria a lista no hmtl  
 function criarTarefa() {
   if (tarefa.value !== '') {
     const criaLista = document.createElement('li');
     criaLista.innerText = tarefa.value;
     tarefa.value = '';
-    listadeTarefa.appendChild(crisLista);
-    criaLista.addEventListener('click', selectedItem);
+    listadeTarefa.appendChild(criaLista);
+    // criaLista.addEventListener('click', selectedItem);
   }
 }
 botao.addEventListener('click', criarTarefa);
