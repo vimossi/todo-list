29625d076f0bc254f707bf43b21aea4264746dfc
ViniciusMossi <vini_agg@hotmail.com>
2021-05-06 16:56:50 -0400

    Verifica novamente requisito 5 e identaçao

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const tarefa = document.getElementById('texto-tarefa');
 const botao = document.getElementById('criar-tarefa');
 const listadeTarefa = document.getElementById('lista-tarefas');
 
-function criarTarefa () {
-  if(tarefa.value !== '') {
+function criarTarefa() {
+  if (tarefa.value !== '') {
     const criaLista = document.createElement('li');
     criaLista.innerText = tarefa.value;
     tarefa.value = '';
     listadeTarefa.appendChild(crisLista);
-    criaLista.addEventListener('click', selectedItem);   
+    criaLista.addEventListener('click', selectedItem);
   }
 }
 botao.addEventListener('click', criarTarefa);
