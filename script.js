8049cd6317f3f518f6d581186e8a26b11b03de66
ViniciusMossi <vini_agg@hotmail.com>
2021-05-06 16:44:01 -0400

    Verifica requisito 5 e estiliza requisitos anteriores

---
+++ script.js
@@ -0,0 +1,14 @@
+const tarefa = document.getElementById('texto-tarefa');
+const botao = document.getElementById('criar-tarefa');
+const listadeTarefa = document.getElementById('lista-tarefas');
+
+function criarTarefa () {
+  if(tarefa.value !== '') {
+    const criaLista = document.createElement('li');
+    criaLista.innerText = tarefa.value;
+    tarefa.value = '';
+    listadeTarefa.appendChild(crisLista);
+    criaLista.addEventListener('click', selectedItem);   
+  }
+}
+botao.addEventListener('click', criarTarefa);
