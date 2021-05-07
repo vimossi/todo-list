5474300aa0cae799a97a69dab3b57e3c32121ea1
ViniciusMossi <vini_agg@hotmail.com>
2021-05-06 23:00:39 -0400

    Verifica requisito 7 e 8

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const tarefa = document.getElementById('texto-tarefa');
 const botao = document.getElementById('criar-tarefa');
 const listadeTarefa = document.getElementById('lista-tarefas');
 
+function itemSelecionado(event) {
+  const poeCor = document.querySelector('.mudaDeCor');
+  if (poeCor !== null) {
+    poeCor.classList.remove('mudaDeCor');
+  }
+  event.target.classList.add('mudaDeCor');
+}
+
+
 // cria a lista no hmtl  
 function criarTarefa() {
   if (tarefa.value !== '') {
@@ -9,7 +18,7 @@ function criarTarefa() {
     criaLista.innerText = tarefa.value;
     tarefa.value = '';
     listadeTarefa.appendChild(criaLista);
-    // criaLista.addEventListener('click', itemSelecionado);
+    criaLista.addEventListener('click', itemSelecionado); /* essa ação vai permitir que ao clicar em algum item da lista ele troque de cor, ativando a função ItemSelecionado */
   }
 }
 botao.addEventListener('click', criarTarefa);
